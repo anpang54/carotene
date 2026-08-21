05c70a32123c8c14a5dea3bb0c8c7bd37726f740
anpang54 <anpangpawit2@gmail.com>
2026-08-21 16:34:11 +0700

    15.2: stack

--- src/vm.hpp
+++ src/vm.hpp
@@ -23,8 +23,9 @@ class VM{
 
     public:
 
-        Chunk* chunk;
-        uint8_t* ip;
+        Chunk*         chunk;
+        uint8_t*       ip;
+        vector<double> stack;    // it's easier to iterate over a vector
 
         InterpretResult interpret(Chunk* chunk) {
             this->chunk = chunk;
@@ -40,21 +41,35 @@ class VM{
 
             for (;;){
 
+                // debug trace execution
                 if(DEBUG_TRACE_EXECUTION) {
+                    
+                    cout << "          ";
+                    for(const double& slot: stack) {
+                        cout << "[ ";
+                        printValue(slot);
+                        cout << " ]";
+                    }
+                    cout << '\n';
+
                     this->chunk->disassembleInstruction((int)(this->ip - this->chunk->code.data()));
+
                 }
 
+                // run the instruction
                 uint8_t instruction;
                 switch(instruction = readByte()) {
 
                     case OP_CONSTANT: {
                         double constant = this->chunk->constants[readByte()];
-                        printValue(constant);
-                        cout << '\n';
+                        this->stack.push_back(constant);
                         break;
                     }
 
                     case OP_RETURN: {
+                        printValue(this->stack.back());
+                        this->stack.pop_back();    // apparently pop_back() doesn't return the value
+                        cout << '\n';
                         return INTERPRET_OK;
                     }
 
@@ -63,5 +78,6 @@ class VM{
             }
 
         }
+
 
 };
