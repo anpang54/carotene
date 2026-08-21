88fa3596902eadc8f365c235ca13b7061ae82a4e
anpang54 <anpangpawit2@gmail.com>
2026-08-21 19:37:01 +0700

    fix file reading

--- src/main.cpp
+++ src/main.cpp
@@ -82,7 +82,7 @@ int main(int argc, const char* argv[]) {
     switch(option) {
 
         case ' ':
-            runFile(argv[2]);
+            runFile(filename);
             break;
 
         case 'c':
