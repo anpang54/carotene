455557b0aab6fa3d5ccbaea6c6e9088676ff04e1
anpang54 <anpangpawit2@gmail.com>
2026-09-22 21:51:16 +0700

    get page as html

--- src/stdlib/modules/mediawiki.hpp
+++ src/stdlib/modules/mediawiki.hpp
@@ -47,14 +47,17 @@ nMethod(mediawiki_Wiki, init, {
 
 nMethod(mediawiki_Wiki, get, {
     params({
-        {{OBJ_STRING}, true}
+        {{OBJ_STRING}, true },
+        {{TYPE_BOOL},  false}
     });
 
     WikiData* data = nativeData<WikiData>(vm, self);
     if(data == nullptr) return CaroNull;
 
+    bool getHtml = args.size() >= 2 && isTruthy(args[1]);
+
     // make the request
-    string url = data->url + "/rest.php/v1/page/" + wikiEncodeTitle(asString(args[0])->str);
+    string url = data->url + "/rest.php/v1/page/" + wikiEncodeTitle(asString(args[0])->str) + (getHtml? "/html": "");
     CaroHttp::Response response;
     string error = CaroHttp::request("GET", url, {}, "", CaroHttp::defaultTimeout, response);
 
@@ -64,20 +67,24 @@ nMethod(mediawiki_Wiki, get, {
         return CaroNull;
     }
 
-    // get wikitext
-    GCPause pause;
-    CaroJsonParser parser(response.body);
-    Value parsed = parser.parseValue();
-    if(parser.errored || !isDict(parsed)) {
-        vm->runtimeError("The response contained invalid JSON.");
-        return CaroNull;
+    // return
+    if(getHtml) {
+        return CaroObj(copyString(response.body));
+    } else {
+        // parse json first
+        GCPause pause;
+        CaroJsonParser parser(response.body);
+        Value parsed = parser.parseValue();
+        if(parser.errored || !isDict(parsed)) {
+            vm->runtimeError("The response contained invalid JSON.");
+            return CaroNull;
+        }
+        auto found = asDict(parsed)->data.find(CaroObj(copyString("source")));
+        if(found == asDict(parsed)->data.end() || !isString(found->second)) {
+            vm->runtimeError("The response contained invalid JSON.");
+            return CaroNull;
+        }
+        return found->second;
     }
-    auto found = asDict(parsed)->data.find(CaroObj(copyString("source")));
-    if(found == asDict(parsed)->data.end() || !isString(found->second)) {
-        vm->runtimeError("The response contained invalid JSON.");
-        return CaroNull;
-    }
-    return found->second;
 
 });
-
